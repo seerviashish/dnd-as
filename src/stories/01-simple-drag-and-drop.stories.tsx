0c6092eed42423fd898ec951b2660441c9725557
Ashish Chaudhary <seerviashish17@gmail.com>
2023-08-05 19:35:38 +0530

    feat(storybook): added source for first example

--- src/stories/01-simple-drag-and-drop.stories.tsx
+++ src/stories/01-simple-drag-and-drop.stories.tsx
@@ -14,3 +14,40 @@ const Template: StoryFn<typeof Basic> = (args) => <Basic {...args} />;
 export const Primary = Template.bind({});
 
 Primary.args = {};
+
+Primary.parameters = {
+  docs: {
+    description: {
+      story:
+        'Basic contains simple drag and drop example from drag area to drop area. Here drop area is not contained in drag area.',
+    },
+    source: {
+      code: `
+import { Box } from '@mui/material';
+import React from 'react';
+import { DragAndDrop, DragArea, DragElement, DropArea } from 'dnd-as';
+
+const Basic: React.FC = () => (
+  <DragAndDrop>
+    <DragArea>
+      {({ dragAreaRef }) => (
+        <Box ref={dragAreaRef}>
+          <DragElement>
+            {({ dragElementRef }) => (
+              <Box ref={dragElementRef}>Draggable Element</Box>
+            )}
+          </DragElement>
+          <DropArea>
+            {({ dropAreaRef }) => <Box ref={dropAreaRef}>Drop Area</Box>}
+          </DropArea>
+        </Box>
+      )}
+    </DragArea>
+  </DragAndDrop>
+);
+
+export default Basic;
+`,
+    },
+  },
+};
